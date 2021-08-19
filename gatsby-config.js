e8a355b895155632c538056666f4f774630e4d97
Gabe-Goodman <goodman.g@northeastern.edu>
2021-08-19 17:15:25 +0900

    Added gatsby-source-filesystem

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,28 @@ module.exports = {
     siteUrl: "https://www.yourdomain.tld",
     title: "sc33n3",
   },
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages`
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `${__dirname}/src/pages`
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/pages`
+      },
+    },
+    }
+  ],
 };
