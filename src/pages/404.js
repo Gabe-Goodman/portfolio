c9d75c11db4f733d5519d44b1a048e6642f951c7
Gabe-Goodman <goodman.g@northeastern.edu>
2021-08-21 13:44:49 +0900

    Added prism react renderer

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,5 +1,7 @@
 import * as React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
+import { Container, Content, FeatureImage } from "../components"
+import { H1 } from "../elements"
 
 // styles
 const pageStyles = {
