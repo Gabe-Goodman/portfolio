0d63cd59ac4bf18d69d1996dfd7f4d91c457af55
Gabriel Goodman <GabrielGoodman@Gabriels-MacBook-Pro-2.local>
2021-12-31 18:19:29 +0900

    Add description

--- src/templates/allPosts.js
+++ src/templates/allPosts.js
@@ -1,74 +1,58 @@
-import React from "react"
-import {graphql} from "gatsby"
-import { 
-    Container, 
-    Content, 
-    ContentCard, 
-    FeatureImage, 
-    Pagination,
-    Seo
-} from "../components"
-import { H1, P } from "../elements"
+import React from 'react';
+import { graphql } from 'gatsby';
+import { Container, Content, ContentCard, FeatureImage, Pagination, Seo } from '../components';
+import { H1, P } from '../elements';
 
-const allPosts = ({pageContext, data}) => {
-    const {currentPage, numPages} = pageContext;
-    const isFirst = currentPage === 1;
-    const isLast = currentPage === numPages;
-    const prevPage = currentPage - 1 === 1 ? "/" : `/${currentPage - 1}`;
-    const nextPage = `/${currentPage + 1}`
+const allPosts = ({ pageContext, data }) => {
+  const { currentPage, numPages } = pageContext;
+  const isFirst = currentPage === 1;
+  const isLast = currentPage === numPages;
+  const prevPage = currentPage - 1 === 1 ? '/' : `/${currentPage - 1}`;
+  const nextPage = `/${currentPage + 1}`;
 
-    const posts = data.allMdx.edges
+  const posts = data.allMdx.edges;
 
-    return (
-        <Container>
-            <Seo />
-            <FeatureImage />
-            <Content>
-                <H1 textAlign="center" margin="0 0 1rem 0">
-                    Gabe Goodman
-                </H1>
-                <P color="dark2" textAlign="center">
-                    Professional Portfolio
-                </P>
-                {posts.map(post => (
-                    <ContentCard
-                        key={post.node.frontmatter.slug}
-                        date={post.node.frontmatter.date}
-                        title={post.node.frontmatter.title}
-                        excerpt={post.node.frontmatter.excerpt}
-                        slug={post.node.frontmatter.slug}
-                    />
-                ))}
-            </Content>
-            <Pagination
-                isFirst={isFirst}
-                isLast={isLast}
-                prevPage={prevPage}
-                nextPage={nextPage}
-            />
-        </Container>
-    )
-}
+  return (
+    <Container>
+      <Seo title={'Gabe Goodman'} description={'Welcome to my professional portfolio.'} />
+      <FeatureImage />
+      <Content>
+        <H1 textAlign="center" margin="0 0 1rem 0">
+          Gabe Goodman
+        </H1>
+        <P color="dark2" textAlign="center">
+          Professional Portfolio
+        </P>
+        {posts.map((post) => (
+          <ContentCard
+            key={post.node.frontmatter.slug}
+            date={post.node.frontmatter.date}
+            title={post.node.frontmatter.title}
+            excerpt={post.node.frontmatter.excerpt}
+            slug={post.node.frontmatter.slug}
+          />
+        ))}
+      </Content>
+      <Pagination isFirst={isFirst} isLast={isLast} prevPage={prevPage} nextPage={nextPage} />
+    </Container>
+  );
+};
 
-export default allPosts 
+export default allPosts;
 
 export const pageQuery = graphql`
-    query AllPostsQuery($skip: Int!, $limit: Int!) {
-        allMdx(
-            sort: { fields: frontmatter___date, order: DESC }
-            skip: $skip 
-            limit: $limit
-        ) {
-            edges {
-              node {
-                frontmatter {
-                  slug
-                  title
-                  date(formatString: "MMM DD, YYYY")
-                  excerpt
-                }
-              }
-            }
+  query AllPostsQuery($skip: Int!, $limit: Int!) {
+    allMdx(sort: { fields: frontmatter___date, order: DESC }, skip: $skip, limit: $limit) {
+      edges {
+        node {
+          frontmatter {
+            slug
+            title
+            date(formatString: "MMM DD, YYYY")
+            excerpt
           }
+        }
+      }
     }
-`
+  }
+`;
