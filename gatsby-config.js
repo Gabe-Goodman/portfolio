c2d35755b89a09bd4a970a586d74eded4882a780
Gabriel Goodman <GabrielGoodman@Gabriels-MacBook-Pro-2.local>
2021-12-31 17:52:47 +0900

    Add browser icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,37 +1,47 @@
 module.exports = {
   siteMetadata: {
-    title: "Gabe Goodman",
-    description: "Welcome to my professional portfolio.",
-    url: "",
-    image: "/image.jpg",
-    author: "Gabe Goodman",
+    title: `Gabe Goodman - Professional Portfolio`,
+    description: 'Welcome to my professional portfolio.',
+    url: '',
+    image: '/image.jpg',
+    author: 'Gabe Goodman',
   },
   plugins: [
-      `gatsby-plugin-image`,
-      `gatsby-plugin-sharp`,
-      `gatsby-transformer-sharp`, // Needed for dynamic images
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`, // Needed for dynamic images
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Gabe Goodman - Professional Portfolio`,
+        short_name: `Gabe Goodman`,
+        start_url: `/`,
+        icon: `src/images/logo.png`,
+        display: `standalone`,
+      },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `pages`,
-        path: `${__dirname}/src/pages/`
+        path: `${__dirname}/src/pages/`,
       },
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `posts`,
-        path: `${__dirname}/src/posts/`
+        path: `${__dirname}/src/posts/`,
       },
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images/`
+        path: `${__dirname}/src/images/`,
       },
     },
-    `gatsby-plugin-sharp`, 
+    `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
     {
       resolve: `gatsby-plugin-mdx`,
@@ -50,12 +60,9 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
-        fonts: [
-          `roboto mono`,
-          `muli\:400,400i,700,700i`
-        ],
-          display: "swap",
-      }
-    }
+        fonts: [`roboto mono`, `muli\:400,400i,700,700i`],
+        display: 'swap',
+      },
+    },
   ],
-}
+};
